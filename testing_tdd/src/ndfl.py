edb2a8abf2666595581b1400f4da4d613a317b79
Fedurin Daniil <qucke74@gmail.com>
2026-05-10 16:22:43 +0800

    add last tier

--- testing_tdd/src/ndfl.py
+++ testing_tdd/src/ndfl.py
@@ -1,9 +1,9 @@
 def calculate_ndfl_tax(income):
-    # start, addition, rate
     tiers = [
         (0.0, 0.0, 0.13),
         (2_400_000.0, 312_000.0, 0.15),
         (5_000_000.0, 702_000.0, 0.18),
+	(20_000_000.0, 3_402_000.0, 0.20)
     ]
     for start, addition, rate in tiers[::-1]:
         if income > start:
